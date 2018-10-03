f06e1726c32ffe93efd04f4e3b07955109d929ee
Gebruiker <gebruiker@fedoraws.scauting.local>
2018-10-03 14:46:02 +0200

    Fixed old dependencies

--- config/app.php
+++ config/app.php
@@ -150,7 +150,6 @@ return [
         /*
          * Package Service Providers...
          */
-         Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
