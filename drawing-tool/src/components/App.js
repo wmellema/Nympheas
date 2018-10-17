678e8dbd30faf64732699bb5781ad2ab031314e8
Valentijn Baakman <vbaakman@gmail.com>
2018-10-17 22:29:20 +0200

    add options to add and remove corners

--- drawing-tool/src/components/App.js
+++ drawing-tool/src/components/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
         <Layer>
           <FlexRect x={100} y={100} width={50} height={50} heldKeys={heldKeys} />
           <FlexRect x={200} y={200} width={40} height={30} heldKeys={heldKeys} />
-          <FlexPoly x={250} y={250} corners={5} size={400} />
+          <FlexPoly x={250} y={250} corners={5} size={400} heldKeys={heldKeys} />
         </Layer>
       </Stage>
     )
--- drawing-tool/src/components/FlexPoly.js
+++ drawing-tool/src/components/FlexPoly.js
@@ -7,9 +7,50 @@ import {Anchor} from './common'
 function getRandomPoints (n, size) {
   const {random, floor} = Math
   const arr = []
-  for (let i = 0; i < n; i++) arr.push(floor(random() * size) + 1)
+  for (let i = 0; i < n * 2; i++) arr.push(floor(random() * size) + 1)
   return arr
 }
+
+function getAverage (array, floor = true) {
+  let sum = 0
+  for (const i of array) sum += i
+  let avg = sum / array.length
+  return floor ? Math.floor(avg) : avg
+}
+
+const diff = (a, b) => a > b ? a - b : b - a
+const lth = (a) => a.sort((a, b) => a - b)
+const htl = (a) => a.sort((a, b) => b - a)
+
+const findClosestPoints = (points, x, y) => (
+  Array(points.length / 2)
+    .fill(null)
+    .map((v, i) => i)
+    .map((v) => v * 2)
+    .sort((a, b) => {
+      const da = diff(points[a], x) + diff(points[a + 1], y)
+      const db = diff(points[b], x) + diff(points[b + 1], y)
+      return da - db
+    })
+)
+
+class PersistentListener {
+  constructor (eventName, callback, turnOn = false) {
+    this.eventName = eventName
+    this.callback = callback
+    this.on = this.on.bind(this)
+    this.off = this.off.bind(this)
+    if (turnOn) this.on()
+  }
+  on () {
+    window.addEventListener(this.eventName, this.callback)
+  }
+  off () {
+    window.removeEventListener(this.eventName, this.callback)
+  }
+}
+
+
 
 export default class FlexPoly extends PureComponent {
   constructor (props) {
@@ -17,17 +58,46 @@ export default class FlexPoly extends PureComponent {
     this.state = {
       x: props.x,
       y: props.y,
-      points: getRandomPoints(props.corners * 2, props.size),
+      points: getRandomPoints(props.corners, props.size),
       fill: props.fill || Konva.Util.getRandomColor()
     }
     this.reposition = this.reposition.bind(this)
     this.resize = this.resize.bind(this)
+    this.trackMouse = this.trackMouse.bind(this)
+    this.keyListener = new PersistentListener('keypress', (event) => {
+      if (event.key === '+') this.addCorner()
+      else if (event.key === '-') this.deleteCorner()
+    })
+  }
+
+  addCorner () {
+    const points = this.state.points.slice()
+    const [a, b] = findClosestPoints(points, ...this.mousePosition)
+    let i
+    if (diff(a, b) === 2) i = htl([a, b])[0]
+    else i = lth([a, b])[0]
+    points.splice(i, 0, ...this.mousePosition)
+    this.setState({points})
+  }
+
+  deleteCorner () {
+    if (this.state.points.length < 7) return
+    const points = this.state.points.slice()
+    const i = findClosestPoints(points, ...this.mousePosition)[0]
+    points.splice(i, 2)
+    this.setState({points})
   }
 
   reposition (event) {
     if (event.target.nodeType !== 'Group') return
     const {x, y} = event.target.attrs
     this.setState({x, y})
+  }
+
+  trackMouse (event) {
+    const {x, y} = this.state
+    const {evt} = event
+    this.mousePosition = [evt.layerX - x, evt.layerY - y]
   }
 
   resize (event) {
@@ -41,7 +111,6 @@ export default class FlexPoly extends PureComponent {
 
   render () {
     const {x, y, points, fill} = this.state
-
     const anchors = []
     for (let i = 0; i < points.length; i += 2) {
       anchors.push(
@@ -54,8 +123,13 @@ export default class FlexPoly extends PureComponent {
     }
 
     return (
-      <Group draggable={true} onDragEnd={this.reposition} x={x} y={y}>
-        <Line closed={true} points={points} fill={fill} shadowBlur={5} />
+      <Group draggable={true} x={x} y={y} onDragEnd={this.reposition}>
+        <Line
+          closed={true} points={points} fill={fill} shadowBlur={5}
+          onMouseEnter={this.keyListener.on}
+          onMouseLeave={this.keyListener.off}
+          onMouseMove={this.trackMouse}
+        />
         {anchors}
       </Group>
     )
